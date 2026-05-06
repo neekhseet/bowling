e1d03f3b3ec10828c4eceb36ff015ca06bf6b5ec
alex <iteeshkeen@gmail.com>
2026-05-06 16:02:53 +0500

    test: every throw equal one point

--- tests/test_game.cpp
+++ tests/test_game.cpp
@@ -7,3 +7,10 @@ TEST(GameTest, GutterGame) {
     game.roll(0);
   EXPECT_EQ(game.score(), 0);
 }
+
+TEST(GameTest, AllOnes) {
+  Game game;
+  for (int i = 0; i < 20; i++)
+    game.roll(1);
+  EXPECT_EQ(game.score(), 20);
+}
