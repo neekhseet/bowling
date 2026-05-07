ca695b6db4963d0c1350ba8565489641aab88af6
alex <iteeshkeen@gmail.com>
2026-05-07 15:37:11 +0500

    feat: strike bonus scoring

--- sources/game.cpp
+++ sources/game.cpp
@@ -3,10 +3,16 @@
 void Game::roll(int pins) { rolls.push_back(pins); };
 
 int Game::score() const {
-  int total;
-
-  for (int roll : rolls) {
-    total += roll;
+  int total = 0;
+  int rollIndex = 0;
+  for (int frame = 0; frame < 10; ++frame) {
+    if (rolls[rollIndex] == 10) {
+      total += 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+      rollIndex++;
+    } else {
+      total += rolls[rollIndex] + rolls[rollIndex + 1];
+      rollIndex += 2;
+    }
   }
 
   return total;
