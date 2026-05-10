7b508afc1971120d1b5576c71fe7a8b5eac83c2a
alex <iteeshkeen@gmail.com>
2026-05-10 22:05:10 +0500

    feat: replace hardcore to consts

--- sources/game.cpp
+++ sources/game.cpp
@@ -5,20 +5,20 @@ void Game::roll(int pins) { rolls.push_back(pins); };
 int Game::score() const {
   int total = 0;
   int rollIndex = 0;
-  for (int frame = 0; frame < 10; ++frame) {
+  for (int frame = 0; frame < MAX_FRAMES; ++frame) {
     if (rollIndex >= rolls.size())
       break;
 
-    if (frame == 9) {
-      if (rolls[rollIndex] == 10) {
+    if (frame == MAX_FRAMES - 1) {
+      if (rolls[rollIndex] == MAX_PINS) {
         if (rollIndex + 2 >= rolls.size())
           break;
-        total += 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+        total += MAX_PINS + rolls[rollIndex + 1] + rolls[rollIndex + 2];
       } else if (rollIndex + 1 < rolls.size() &&
-                 rolls[rollIndex] + rolls[rollIndex + 1] == 10) {
+                 rolls[rollIndex] + rolls[rollIndex + 1] == MAX_PINS) {
         if (rollIndex + 2 >= rolls.size())
           break;
-        total += 10 + rolls[rollIndex + 2];
+        total += MAX_PINS + rolls[rollIndex + 2];
       } else if (rollIndex + 1 < rolls.size()) {
         total += rolls[rollIndex] + rolls[rollIndex + 1];
       } else {
@@ -27,16 +27,16 @@ int Game::score() const {
       break;
     }
 
-    if (rolls[rollIndex] == 10) {
+    if (rolls[rollIndex] == MAX_PINS) {
       if (rollIndex + 2 >= rolls.size())
         break;
       total += 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
       rollIndex += 1;
     } else if (rollIndex + 1 < rolls.size() &&
-               rolls[rollIndex] + rolls[rollIndex + 1] == 10) {
+               rolls[rollIndex] + rolls[rollIndex + 1] == MAX_PINS) {
       if (rollIndex + 2 >= rolls.size())
         break;
-      total += 10 + rolls[rollIndex + 2];
+      total += MAX_PINS + rolls[rollIndex + 2];
       rollIndex += 2;
     } else if (rollIndex + 1 < rolls.size()) {
       total += rolls[rollIndex] + rolls[rollIndex + 1];
@@ -55,7 +55,7 @@ int Game::frameScore(int frame) const {
   for (int f = 1; f < frame; ++f) {
     if (rollIndex >= rolls.size())
       return 0;
-    if (rolls[rollIndex] == 10)
+    if (rolls[rollIndex] == MAX_PINS)
       rollIndex += 1;
     else
       rollIndex += 2;
@@ -64,19 +64,19 @@ int Game::frameScore(int frame) const {
   if (rollIndex >= rolls.size())
     return 0;
 
-  if (rolls[rollIndex] == 10) {
+  if (rolls[rollIndex] == MAX_PINS) {
     if (rollIndex + 2 >= rolls.size())
       return 0;
-    return 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+    return MAX_PINS + rolls[rollIndex + 1] + rolls[rollIndex + 2];
   }
 
   if (rollIndex + 1 >= rolls.size())
     return 0;
 
-  if (rolls[rollIndex] + rolls[rollIndex + 1] == 10) {
+  if (rolls[rollIndex] + rolls[rollIndex + 1] == MAX_PINS) {
     if (rollIndex + 2 >= rolls.size())
       return 0;
-    return 10 + rolls[rollIndex + 2];
+    return MAX_PINS + rolls[rollIndex + 2];
   }
 
   return rolls[rollIndex] + rolls[rollIndex + 1];
@@ -88,7 +88,7 @@ int Game::rollScore(int frame, int roll) const {
   for (int f = 1; f < frame; ++f) {
     if (rollIndex >= rolls.size())
       return 0;
-    if (rolls[rollIndex] == 10)
+    if (rolls[rollIndex] == MAX_PINS)
       rollIndex += 1;
     else
       rollIndex += 2;
@@ -97,7 +97,7 @@ int Game::rollScore(int frame, int roll) const {
   if (rollIndex >= rolls.size())
     return 0;
 
-  if (roll == 2 && rolls[rollIndex] == 10)
+  if (roll == 2 && rolls[rollIndex] == MAX_PINS)
     return 0;
 
   if (rollIndex + (roll - 1) >= rolls.size())
