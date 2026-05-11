f87e38f1b0525875a1097c9d1dbdc62db496799c
alex <iteeshkeen@gmail.com>
2026-05-11 22:56:42 +0500

    fix: miss strike hardcore number, replaced by MAX_PINS

--- sources/game.cpp
+++ sources/game.cpp
@@ -30,7 +30,7 @@ int Game::score() const {
     if (rolls[rollIndex] == MAX_PINS) {
       if (rollIndex + 2 >= rolls.size())
         break;
-      total += 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+      total += MAX_PINS + rolls[rollIndex + 1] + rolls[rollIndex + 2];
       rollIndex += 1;
     } else if (rollIndex + 1 < rolls.size() &&
                rolls[rollIndex] + rolls[rollIndex + 1] == MAX_PINS) {
