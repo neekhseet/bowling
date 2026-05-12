0cf1759481eedb0330e68b17b40f96d7fee7b42e
alex <iteeshkeen@gmail.com>
2026-05-12 17:15:24 +0500

    feat: added throw logic error for frameScore

--- sources/game.cpp
+++ sources/game.cpp
@@ -54,9 +54,11 @@ int Game::frameScore(int frame) const {
   int rollIndex = startFrameIndex(frame);
 
   if (rollIndex < 0 || rollIndex >= rolls.size())
+    throw std::logic_error("Not enough rolls for calculate this frame.");
+
+  if (rolls[rollIndex] != MAX_PINS && rollIndex + 1 >= rolls.size()) {
     return 0;
-  if (rolls[rollIndex] != MAX_PINS && rollIndex + 1 >= rolls.size())
-    return 0;
+  }
 
   return scoreFrameInternal(rollIndex);
 }
